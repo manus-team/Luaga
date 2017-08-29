591ea8d42e5993c7f3a20b594061ffc5b481ca9d
noahsmindfuck <arche1@gmx.at>
2017-08-29 15:26:47 +0200

    Update camera_module_listen.py

--- camera_module_listen.py
+++ camera_module_listen.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 		if command == b'capture':
 			print("Capturing image")
 			
-#			filename = os.path.join(DESTINATION_PATH, "%s.jpg" % socket.gethostname())
+			filename = os.path.join(DESTINATION_PATH, "%s.jpg" % socket.gethostname())
 			temp_filename = os.path.join(TEMP_PATH, "%s.jpg" % socket.gethostname())
 			if subprocess.call(['raspistill', '-w', '3280', '-h', '2464', '-o', temp_filename]) == 0:
 				print("Saved image to", temp_filename)
