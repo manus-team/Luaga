71ea165f1d953e65e832cb2028fb3e48dfd4d4b5
Roman Zimmermann <roman@more-onion.com>
2017-08-29 20:36:59 +0200

    Whitespace.

--- camera_module_listen.py
+++ camera_module_listen.py
@@ -14,25 +14,25 @@ DESTINATION_PATH = '/home/pi/scans/'
 TEMP_PATH = '/home/pi/'
 
 if __name__ == '__main__':
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only to MCAST_GRP, not all groups on MCAST_PORT
-	mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only to MCAST_GRP, not all groups on MCAST_PORT
+    mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
-	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-	
-	while True:
-		command = sock.recv(1024)
-		if command == b'capture':
-			print("Capturing image")
-			
-			filename = os.path.join(DESTINATION_PATH, "%s.jpg" % socket.gethostname())
-			temp_filename = os.path.join(TEMP_PATH, "%s.jpg" % socket.gethostname())
-			if subprocess.call(['raspistill', '-w', '3280', '-h', '2464', '-o', temp_filename]) == 0:
-				print("Saved image to", temp_filename)
-				if subprocess.call(['rsync', '-avz', '--temp-dir=/home/pi/', temp_filename, DESTINATION_PATH]) == 0:
-					print("Copied Image to", filename)
-				else:
-					print("copying failed")
-			else:
-				print("Photograpy failed")
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        command = sock.recv(1024)
+        if command == b'capture':
+            print("Capturing image")
+
+            filename = os.path.join(DESTINATION_PATH, "%s.jpg" % socket.gethostname())
+            temp_filename = os.path.join(TEMP_PATH, "%s.jpg" % socket.gethostname())
+            if subprocess.call(['raspistill', '-w', '3280', '-h', '2464', '-o', temp_filename]) == 0:
+                print("Saved image to", temp_filename)
+                if subprocess.call(['rsync', '-avz', '--temp-dir=/home/pi/', temp_filename, DESTINATION_PATH]) == 0:
+                    print("Copied Image to", filename)
+                else:
+                    print("copying failed")
+            else:
+                print("Photograpy failed")
