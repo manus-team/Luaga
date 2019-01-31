6ad8c30191b12c9c87f6cad2ca00f54a0a19ca81
Barnaby Walters <barnaby@waterpigs.co.uk>
2019-01-31 15:59:50 +0100

    Fixed bug in debugging code

--- camera_module_listen.py
+++ camera_module_listen.py
@@ -20,14 +20,17 @@ if __name__ == '__main__':
     print("Opening multicast listening socket.")
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only to MCAST_GRP, not all groups on MCAST_PORT
+    
+    # use MCAST_GRP instead of '' to listen only to MCAST_GRP, not all groups on MCAST_PORT
+    sock.bind(('', MCAST_PORT))
+    
     mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     while True:
         command = sock.recv(1024)
-        print("Received multicast command: %s" % command.encode('utf-8'))
+        print("Received multicast command: %s" % command)
         if command == b'capture':
             print("Capturing image")
 
